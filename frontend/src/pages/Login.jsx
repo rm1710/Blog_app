945e37bf9506b59b84ddc2ad71d21532927cf538
rm1710 <d290mishraritesh@gmail.com>
2025-02-23 21:16:54 +0530

    new

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -30,6 +30,7 @@ function Login() {
       );
       console.log(data);
       toast.success(data.message ||'User Logined Successfully');
+      localStorage.setItem('jwt', data.token);
       setIsAuthenticated(true);
       setEmail('');
       setPassword('');
