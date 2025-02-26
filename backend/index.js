d503d3436698b480ae0ccbe03a075a495c20915f
Ritesh Sunil Mishra <162662643+rm1710@users.noreply.github.com>
2025-02-26 18:45:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,8 @@ app.use(
   cors({
     origin: FRONTEND_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(fileUpload({
